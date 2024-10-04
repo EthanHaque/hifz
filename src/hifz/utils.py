f138294d27b1845a1fed446014b929f073533731
ethanhaque <43977335+EthanHaque@users.noreply.github.com>
2024-10-04 16:47:55 -0400

    add: context class for flashcard sessions

--- src/hifz/utils.py
+++ src/hifz/utils.py
@@ -1,7 +1,18 @@
 from dataclasses import dataclass
 
+from learning_strategies import FlashcardStrategy
+
 
 @dataclass
 class Card:
     front: str
     back: str
+
+
+class FlashcardSession:
+    def __init__(self, flashcards: list[Card], strategy: FlashcardStrategy):
+        self.flashcards = flashcards
+        self.strategy = strategy
+
+    def next_flashcard(self) -> Card:
+        return self.strategy.get_next_flashcard(self.flashcards)
