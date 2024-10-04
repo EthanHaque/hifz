48557f2990e41dd8401255a23bffd09faa3b2ead
ethanhaque <43977335+EthanHaque@users.noreply.github.com>
2024-10-04 16:44:41 -0400

    add: concrete sequential strategy implementation

--- src/hifz/learning_strategies.py
+++ src/hifz/learning_strategies.py
@@ -13,3 +13,13 @@ class FlashcardStrategy(ABC):
 class RandomStrategy(FlashcardStrategy):
     def get_next_flashcard(self, flashcards: list[Card]) -> Card:
         return random.choice(flashcards)
+
+
+class SequentialStrategy(FlashcardStrategy):
+    def __init__(self):
+        self.index = 0
+
+    def get_next_flashcard(self, flashcards: list[Card]) -> Card:
+        card = flashcards[self.index]
+        self.index = (self.index + 1) % len(flashcards)
+        return card
