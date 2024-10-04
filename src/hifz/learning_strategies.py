551c6802024998faa01bf368cd2d1579065da531
ethanhaque <43977335+EthanHaque@users.noreply.github.com>
2024-10-04 16:43:14 -0400

    add: type hints on parameters

--- src/hifz/learning_strategies.py
+++ src/hifz/learning_strategies.py
@@ -6,10 +6,10 @@ from utils import Card
 
 class FlashcardStrategy(ABC):
     @abstractmethod
-    def get_next_flashcard(self, flashcards) -> Card:
+    def get_next_flashcard(self, flashcards: list[Card]) -> Card:
         pass
 
 
 class RandomStrategy(FlashcardStrategy):
-    def get_next_flashcard(self, flashcards) -> Card:
+    def get_next_flashcard(self, flashcards: list[Card]) -> Card:
         return random.choice(flashcards)
