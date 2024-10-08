ceb384dbdc45a57db70111e5c3e43dc7377a5f64
Rafay Khan <82721722+rafaykhan-source@users.noreply.github.com>
2024-10-08 18:43:05 -0400

    refactor: rename flashcards to cards (#11)

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,18 +1,18 @@
 from hifz.learning_strategies import RandomStrategy, SequentialStrategy
 from hifz.models import Card
-from hifz.utils import FlashcardSession
+from hifz.utils import CardSession
 
 
-def test_flashcard_session_with_random_strategy(flashcards: list[Card]):
+def test_card_session_with_random_strategy(cards: list[Card]):
     random_strategy = RandomStrategy()
-    session = FlashcardSession(flashcards, random_strategy)
-    card = session.next_flashcard()
-    assert card in flashcards
+    session = CardSession(cards, random_strategy)
+    card = session.next_card()
+    assert card in cards
 
 
-def test_flashcard_session_with_sequential_strategy(flashcards: list[Card]):
+def test_card_session_with_sequential_strategy(cards: list[Card]):
     sequential_strategy = SequentialStrategy()
-    session = FlashcardSession(flashcards, sequential_strategy)
+    session = CardSession(cards, sequential_strategy)
 
-    assert session.next_flashcard() == flashcards[0]
-    assert session.next_flashcard() == flashcards[1]
+    assert session.next_card() == cards[0]
+    assert session.next_card() == cards[1]
