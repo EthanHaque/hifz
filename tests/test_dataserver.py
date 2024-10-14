2467d18df0e6125f78aeaeb52a256b7f1238d8b2
Callum Boyagoda <80609708+BoyagodaC@users.noreply.github.com>
2024-10-13 21:57:44 -0400

    Bug: switched to tmp_path_factory and session fixture

--- tests/test_dataserver.py
+++ tests/test_dataserver.py
@@ -3,19 +3,19 @@ from pathlib import Path
 from hifz.dataserver import DataServer
 from hifz.models import Card
 
-
-def test_dataserver_returns_card_csv(tmp_file):
+@pytest.fixture(scope="session")
+def test_dataserver_returns_card_csv(tmp_path_factory):
     server = DataServer()
-    tmp_file_path = tmp_file / "test_arabic.csv"
+    tmp_file_path = tmp_path_factory.mktemp("test_arabic.csv")
     with Path(tmp_file_path).open(encoding="uft-8") as f:
         f.writelines(["front,back", "ب,baa"])
     cards = server.read_entries(tmp_file_path)
     assert Card("ب", "baa'") in cards
 
-
+@pytest.fixture(scope="session")
 def test_dataserver_returns_card_json(tmp_file):
     server = DataServer()
-    tmp_file_path = tmp_file / "test_arabic.json"
+    tmp_file_path = tmp_path_factory.mktemp("test_arabic.json")
     with Path(tmp_file_path).open(encoding="uft-8") as f:
         f.write(str([{"front": "ب", "back": "baa"}]))
     cards = server.read_entries(tmp_file_path)
