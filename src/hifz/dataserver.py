1d6231a24f1944fcd8506ceafa88aec3f98d7b4c
Callum Boyagoda <80609708+BoyagodaC@users.noreply.github.com>
2024-10-13 22:05:34 -0400

    feat: json dataloading and json/csv temp data test (#12)
    * feat: json dataloading and json/csv temp data test
    
    * Moved card loading outside if-else
    
    Co-authored-by: Rafay Khan <82721722+rafaykhan-source@users.noreply.github.com>
    
    * fixed csv_test's temp file data
    
    Co-authored-by: Rafay Khan <82721722+rafaykhan-source@users.noreply.github.com>
    
    * Updated temp data for csv test
    
    Co-authored-by: Rafay Khan <82721722+rafaykhan-source@users.noreply.github.com>
    
    * Update: used tmp_path fixture and added filetype err
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * bug: added encoding at test_dataserver and fixed var name changes at csv/json
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * add: ci.yml and dsstore to gitignore
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Bug: switched to tmp_path_factory and session fixture
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Bug: imported pytest
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Bug: fixed tmp_path_factory
    
    ---------
    
    Co-authored-by: Rafay Khan <82721722+rafaykhan-source@users.noreply.github.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/hifz/dataserver.py
+++ src/hifz/dataserver.py
@@ -1,4 +1,5 @@
 import csv
+import json
 from pathlib import Path
 
 from hifz.models import Card
@@ -15,10 +16,18 @@ class DataServer:
             list[Card]: The list of cards.
         """
         cards = []
-
+        types_accepted = [".csv", ".json"]
         try:
-            with Path(file_path).open(encoding="utf-8") as f:
-                entries = csv.DictReader(f)
+            with Path(file_path).open(encoding="uft-8") as f:
+                try:
+                    if file_path.lower.endswith(types_accepted[0]):
+                        entries = csv.DictReader(f)
+                    elif file_path.endswith(types_accepted[1]):
+                        entries = json.load(f)
+                except OSError as err:
+                    error_message = f"Error: The file type at path '{file_path}' is not supported.\
+                    please use {",".join(types_accepted)}"
+                    raise OSError(error_message) from err
                 cards = [Card(e["front"], e["back"]) for e in entries]
         except FileNotFoundError as err:
             error_message = f"Error: The file at path '{file_path}' was not found."
