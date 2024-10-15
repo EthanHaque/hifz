496ec25f97c2f69cb9f1453637cba7c8502e93a1
= <43977335+EthanHaque@users.noreply.github.com>
2024-10-14 22:55:22 -0400

    add: noqa directives to ignore T201 print found

--- src/hifz/visualizers.py
+++ src/hifz/visualizers.py
@@ -21,13 +21,13 @@ class CardInterface(ABC):
 
 class TUICardInterface(CardInterface):
     def display_card_front(self, card: Card) -> None:
-        print(f"\nFront: {card.front}")
+        print(f"\nFront: {card.front}") # noqa: T201
 
     def display_card_back(self, card: Card) -> None:
-        print(f"Back: {card.back}")
+        print(f"Back: {card.back}") # noqa: T201
 
     def get_user_input(self, prompt: str) -> str:
         return input(prompt)
 
     def notify(self, message: str) -> None:
-        print(message)
+        print(message) # noqa: T201
