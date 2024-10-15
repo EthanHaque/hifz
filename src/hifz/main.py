1cb2160f151733e6debc04ef42f3740d47452c6d
Ethan Haque <43977335+EthanHaque@users.noreply.github.com>
2024-10-15 19:09:57 -0400

    refactor: using argparse to make basic cli app

--- src/hifz/main.py
+++ src/hifz/main.py
@@ -1,49 +1,48 @@
+import argparse
+
 from hifz.card_engine import CardEngine
 from hifz.dataserver import DataServer
-from hifz.learning_strategies import RandomStrategy, SequentialStrategy
+from hifz.learning_strategies import CardStrategy, RandomStrategy, SequentialStrategy
+from hifz.models import Card
 from hifz.visualizers import TUICardInterface
 
 
-def load_cards():
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filepath", help="Path to card file")
+    parser.add_argument("strategy", help="Learning strategy")
+    return parser.parse_args()
+
+
+def load_cards(file_path: str) -> list[Card]:
     data_server = DataServer()
-    file_path = input("Enter the path to your card file (CSV/JSON): ").strip()
 
     try:
-        cards = data_server.read_entries(file_path)
-        print(f"Loaded {len(cards)} cards.")  # noqa: T201
-        return cards
-    except Exception as e:
-        print(f"Error: {e}")  # noqa: T201
+        return data_server.read_entries(file_path)
+    except Exception as _:
         return []
 
 
-def choose_strategy():
-    print("\nChoose a learning strategy:")  # noqa: T201
-    print("1. Random")  # noqa: T201
-    print("2. Sequential")  # noqa: T201
-    choice = input("Enter your choice (1 or 2): ").strip()
-
-    if choice == "1":
-        return RandomStrategy()
-    if choice == "2":
-        return SequentialStrategy()
-    print("Invalid choice, defaulting to Sequential.")  # noqa: T201
-    return SequentialStrategy()
+def get_strategy(strategy: str) -> CardStrategy:
+    match strategy:
+        case "Random":
+            return RandomStrategy()
+        case "Sequential":
+            return SequentialStrategy()
+        case _:
+            error_message = f"{strategy} is not a valid strategy"
+            raise ValueError(error_message)
 
 
 def main():
-    print("Welcome to hifz")  # noqa: T201
+    args = parse_args()
 
-    cards = load_cards()
-    if not cards:
-        print("No cards loaded. Exiting...")  # noqa: T201
-        return
-
-    strategy = choose_strategy()
-
+    cards = load_cards(args.filepath)
+    strategy = get_strategy(args.strategy)
     interface = TUICardInterface()
 
     engine = CardEngine(cards, strategy, interface)
+
     engine.run()
 
 
