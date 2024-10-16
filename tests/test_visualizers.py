b83804097b2e0e4f9b0d9039072ff17b13b082af
Ethan Haque <43977335+EthanHaque@users.noreply.github.com>
2024-10-16 00:52:11 -0400

    fix: using pytest-mock instead of unittest mock

--- tests/test_visualizers.py
+++ tests/test_visualizers.py
@@ -1,22 +1,20 @@
-from unittest.mock import patch
-
 from hifz.models import Card
 from hifz.visualizers.cli import CLICardInterface
 
 
-def test_cli_display_card_front():
+def test_cli_display_card_front(mocker):
     interface = CLICardInterface()
     card = Card("Hello", "World")
 
-    with patch("builtins.print") as mocked_print:
-        interface.display_card_front(card)
-        mocked_print.assert_called_once_with("\nFront: Hello")
+    mocked_print = mocker.patch("builtins.print")
+    interface.display_card_front(card)
+    mocked_print.assert_called_once_with("\nFront: Hello")
 
 
-def test_cli_display_card_back():
+def test_cli_display_card_back(mocker):
     interface = CLICardInterface()
     card = Card("Hello", "World")
 
-    with patch("builtins.print") as mocked_print:
-        interface.display_card_back(card)
-        mocked_print.assert_called_once_with("Back: World")
+    mocked_print = mocker.patch("builtins.print")
+    interface.display_card_back(card)
+    mocked_print.assert_called_once_with("Back: World")
