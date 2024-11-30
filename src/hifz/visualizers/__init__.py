3af855120e575a77c6a6bf4af8051830417c4a38
Ethan Haque <43977335+EthanHaque@users.noreply.github.com>
2024-11-30 14:55:58 -0500

    fix: removing unnecessary interface methods

--- src/hifz/visualizers/__init__.py
+++ src/hifz/visualizers/__init__.py
@@ -3,23 +3,10 @@
 from abc import ABC, abstractmethod
 
 from hifz.card_engine import CardEngine
-from hifz.models import Card
 
 
 class CardInterface(ABC):
     """This ABC represents the CardInterface."""
-
-    @abstractmethod
-    def display_card_front(self, card: Card) -> None:
-        """Displays the card front."""
-
-    @abstractmethod
-    def display_card_back(self, card: Card) -> None:
-        """Displays the card back."""
-
-    @abstractmethod
-    def notify(self, message: str) -> None:
-        """Notifies with a message."""
 
     @abstractmethod
     def run_session(self, engine: CardEngine) -> None:
