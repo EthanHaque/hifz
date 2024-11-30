a44db4fa7c6aa385f9855a05423554f479d89799
Ethan Haque <43977335+EthanHaque@users.noreply.github.com>
2024-11-30 01:07:39 -0500

    add: docs build session

--- noxfile.py
+++ noxfile.py
@@ -1,5 +1,7 @@
 """The nox file for the project."""
 
+import argparse
+
 import nox
 
 
@@ -10,16 +12,44 @@ def tests(session: nox.Session) -> None:
     session.run("pytest", *session.posargs)
 
 
-@nox.session
+@nox.session(reuse_venv=True)
 def docs(session: nox.Session) -> None:
-    """Build the docs. Pass "--serve" to serve."""
-    session.install(".[docs]")
-    session.chdir("docs")
-    session.run("sphinx-build", "-M", "html", ".", "build")
+    """Build the docs. Pass --non-interactive to avoid serving. First positional argument is the target directory."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-b", dest="builder", default="html", help="Build target (default: html)"
+    )
+    parser.add_argument("output", nargs="?", help="Output directory")
+    args, posargs = parser.parse_known_args(session.posargs)
+    serve = args.builder == "html" and session.interactive
+
+    session.install(".[docs]", "sphinx-autobuild")
+
+    shared_args = (
+        # "-n",  # nitpicky mode
+        "-T",  # full tracebacks
+        f"-b={args.builder}",
+        "docs",
+        args.output or f"docs/_build/{args.builder}",
+        *posargs,
+    )
+
+    if serve:
+        session.run("sphinx-autobuild", "--open-browser", *shared_args)
+    else:
+        session.run("sphinx-build", "--keep-going", *shared_args)
 
 
 @nox.session
-def serve(session: nox.Session) -> None:
-    """Serves the documentation."""
-    docs(session)
-    session.run("python", "-m", "http.server", "8000", "-d", "_build/html")
+def build_api_docs(session: nox.Session) -> None:
+    """Build (regenerate) API docs."""
+    session.install(".[docs]")
+    session.run(
+        "sphinx-apidoc",
+        "-o",
+        "docs/api/",
+        "--module-first",
+        "--no-toc",
+        "--force",
+        "src/hifz",
+    )
