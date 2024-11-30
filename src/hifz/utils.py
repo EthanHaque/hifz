3a46475622d2983d110f7192d2d322009510b8bf
Ethan Haque <43977335+EthanHaque@users.noreply.github.com>
2024-11-30 01:29:36 -0500

    fix: getting rid of unused feedback code

--- src/hifz/utils.py
+++ src/hifz/utils.py
@@ -1,5 +1,5 @@
 from hifz.learning_strategies import CardStrategy
-from hifz.models import Card, Feedback
+from hifz.models import Card
 
 
 class CardSession:
@@ -9,7 +9,3 @@ class CardSession:
 
     def next_card(self) -> Card:
         return self.strategy.get_next_card(self.cards)
-
-    def process_feedback(self, card: Card, **kwargs) -> None:
-        feedback = Feedback(kwargs)
-        self.strategy.process_feedback(card, feedback)
