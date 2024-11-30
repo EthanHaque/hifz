d8e10fdfbd3080c2a2959cd4180f2264b145dab9
Ethan Haque <43977335+EthanHaque@users.noreply.github.com>
2024-11-30 01:09:08 -0500

    fix: combining docs build steps into one

--- noxfile.py
+++ noxfile.py
@@ -14,7 +14,7 @@ def tests(session: nox.Session) -> None:
 
 @nox.session(reuse_venv=True)
 def docs(session: nox.Session) -> None:
-    """Build the docs. Pass --non-interactive to avoid serving. First positional argument is the target directory."""
+    """Build API docs and the main docs. Pass --non-interactive to avoid serving. First positional argument is the target directory."""
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "-b", dest="builder", default="html", help="Build target (default: html)"
@@ -25,6 +25,18 @@ def docs(session: nox.Session) -> None:
 
     session.install(".[docs]", "sphinx-autobuild")
 
+    # Generate API documentation
+    session.run(
+        "sphinx-apidoc",
+        "-o",
+        "docs/api/",
+        "--module-first",
+        "--no-toc",
+        "--force",
+        "src/hifz",
+    )
+
+    # Build main documentation
     shared_args = (
         # "-n",  # nitpicky mode
         "-T",  # full tracebacks
@@ -38,18 +50,3 @@ def docs(session: nox.Session) -> None:
         session.run("sphinx-autobuild", "--open-browser", *shared_args)
     else:
         session.run("sphinx-build", "--keep-going", *shared_args)
-
-
-@nox.session
-def build_api_docs(session: nox.Session) -> None:
-    """Build (regenerate) API docs."""
-    session.install(".[docs]")
-    session.run(
-        "sphinx-apidoc",
-        "-o",
-        "docs/api/",
-        "--module-first",
-        "--no-toc",
-        "--force",
-        "src/hifz",
-    )
