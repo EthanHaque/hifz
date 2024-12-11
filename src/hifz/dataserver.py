68b65dbcd263f008735dd217f3efb062b32e3d91
Rafay <82721722+rafaykhan-source@users.noreply.github.com>
2024-12-11 16:50:54 -0500

    add: xml parsing strategy

--- src/hifz/dataserver.py
+++ src/hifz/dataserver.py
@@ -2,6 +2,7 @@
 
 import csv
 import json
+import xml.dom.minidom as xml
 from abc import ABC, abstractmethod
 from pathlib import Path
 
@@ -36,6 +37,39 @@ class JSONFileInputStrategy(FileInputStrategy):
             return [Card(e["front"], e["back"]) for e in entries]
 
 
+class XMLFileInputStrategy(FileInputStrategy):
+    """This class maintains the logic for reading from XML files types."""
+
+    def read_entries(self, file_path: Path) -> list[Card]:
+        """Reads the entries from the XML at file_path."""
+        with file_path.open("r", encoding="utf-8") as f:
+            dom = xml.parse(f)
+
+        cards = []
+        for card in dom.getElementsByTagName("card"):
+            front_element = card.getElementsByTagName("front")[0].firstChild
+            if not front_element:
+                msg = f"Missing card front in {file_path}"
+                raise ValueError(msg)
+            if not isinstance(front_element, xml.Text):
+                msg = "Front node is not of type Text"
+                raise ValueError(msg)
+
+            back_element = card.getElementsByTagName("back")[0].firstChild
+            if not back_element:
+                msg = f"Missing card back in {file_path}"
+                raise ValueError(msg)
+            if not isinstance(back_element, xml.Text):
+                msg = "Back node is not of type Text"
+                raise ValueError(msg)
+
+            front_text = front_element.nodeValue
+            back_text = back_element.nodeValue
+
+            cards.append(Card(front_text, back_text))
+        return cards
+
+
 class FileInputReader:
     """Dispatches the appropriate reader strategy based on file extension."""
 
@@ -44,6 +78,7 @@ class FileInputReader:
         self.strategies = {
             ".csv": CSVFileInputStrategy(),
             ".json": JSONFileInputStrategy(),
+            ".xml": XMLFileInputStrategy(),
         }
 
     def get_strategy(self, file_extension: str) -> FileInputStrategy:
